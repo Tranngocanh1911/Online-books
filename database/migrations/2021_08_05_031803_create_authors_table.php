f61f02d943f4fb7fc0a109b7a85955ea860764f2
nguyentuyen348 <nguyentuyen348@gmail.com>
2021-08-06 11:43:59 +0700

    CRUD_author_category_book

--- database/migrations/2021_08_05_031803_create_authors_table.php
+++ database/migrations/2021_08_05_031803_create_authors_table.php
@@ -15,7 +15,7 @@ class CreateAuthorsTable extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
-            $table->string('image')->default('default.jpg');
+            $table->string('image')->nullable();
             $table->string('name');
             $table->string('date_of_birth');
             $table->string('address');
